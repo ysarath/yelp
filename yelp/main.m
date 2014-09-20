161d416e3016ae7bc4ae7f8af97a0af6754a9f0a
ysarath <sarath@snv-loan50-lm.corp.yahoo.com>
2014-09-19 17:50:07 -0700

    Initial Commit

---
+++ yelp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yelp
+//
+//  Created by Sarath Yalamanchili on 9/19/14.
+//  Copyright (c) 2014 Sarath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
